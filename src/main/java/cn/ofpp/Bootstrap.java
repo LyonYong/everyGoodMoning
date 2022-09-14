783f4dd6c87992076a562dacd227e49bab897ef5
Lyon <83961183+LyonYong@users.noreply.github.com>
2022-09-14 10:17:46 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx5fcacb955e08f47c";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "076a4ac9fa261f4f7c76c820d9de289f";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "y1j8qAUi_OBZFobohUK13EKeUbVj8oVctGgK4escvfM";
 
     /**
      * 初始化
